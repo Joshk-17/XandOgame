b80b1c013e2afa2e0001df04437608fabee6c444
Joshua <C23333113@mytudublin.ie>
2026-08-31 19:36:54 +0100

    Made a board for game on the web app

--- src/main/java/WebApp.java
+++ src/main/java/WebApp.java
@@ -7,7 +7,37 @@ public class WebApp {
         Javalin app = Javalin.create(config -> {
 
             config.routes.get("/", ctx -> {
-                ctx.result("Xs and Os Web App");
+                String html = """
+                <html>
+                    <head>
+                        <title>Xs and Os</title>
+                    </head>
+
+                    <body>
+                        <h1>Xs and Os</h1>
+
+                        <table border="1">
+                            <tr>
+                                <td>7</td>
+                                <td>8</td>
+                                <td>9</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>5</td>
+                                <td>6</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>2</td>
+                                <td>3</td>
+                            </tr>
+                        </table>
+
+                    </body>
+                    </html> 
+                    """;
+                ctx.html(html);
             });
 
         }).start(7070);
